853c1b72f9be771b53602c3ac5d0beef3fac1e1c
er-ssmohanty <pie2001@protonmail.com>
2022-02-20 12:29:22 +0530

    add the 3rd hello world

--- buttoned.py
+++ buttoned.py
@@ -8,5 +8,7 @@ def helloCallBack():
    msg = messagebox.showinfo( "Hello Python", "Hello World")
 
 B = Button(top, text = "Hello", command = helloCallBack)
+#ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
 B.place(x = 50,y = 50)
 top.mainloop()
